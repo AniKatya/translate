3f6ac7e0c7a2c37886782e477330229f53479cea
Anikatya <lopukhina.ekaterina@gmail.com>
2020-06-21 16:10:11 +0300

    added context object with default value

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends Component {
+  static contextType = LanguageContext; //= Button.contextType=LanguageContext
+
   render() {
-    return <button className="ui button primary">Submit</button>;
+    let text;
+    if (this.context === "english") {
+      text = "Submit";
+    } else if (this.context === "hebrew") {
+      text = "לשלוח";
+    } else if (this.context === "russian") {
+      text = "Отправить";
+    }
+    return <button className="ui button primary">{text}</button>;
   }
 }
 export default Button;
--- src/components/Field.js
+++ src/components/Field.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
-class Field extends React.Component{
-    render(){
-        return (
-            <div className="ui field">
-                <label>Name</label>
-                <input></input>
-            </div>
-        )
+class Field extends React.Component {
+  static contextType = LanguageContext;
+  render() {
+    let text;
+    if (this.context === "english") {
+      text = "Name";
+    } else if (this.context === "hebrew") {
+      text = "שם";
+    } else if (this.context === "russian") {
+      text = "Имя";
     }
+    return (
+      <div className="ui field">
+        <label>{text}</label>
+        <input></input>
+      </div>
+    );
+  }
 }
 
 export default Field;
